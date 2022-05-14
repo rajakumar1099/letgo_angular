e55d8b5472f8737599bd63e667d6a72a9c87454b
Rajakumar1099 <rajakumarsaravanan@gmail.com>
2022-05-14 16:37:42 +0530

    build fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAbGNyUNwShKp4Njh9fzGdp5bI_Nigjx7Y',
+    authDomain: 'joysale-d8633.firebaseapp.com',
+    projectId: 'joysale-d8633',
+    storageBucket: 'joysale-d8633.appspot.com',
+    messagingSenderId: '822861984582',
+    appId: '1:822861984582:web:e0ab747cbb9173a6b12b47',
+    measurementId: 'G-7ZRY528SZT',
+  }
 };
